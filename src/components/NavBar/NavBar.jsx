3eb7f54f699cdc7afde5c7637d4f1df7fcd818ff
DiegoParanha <DiegoParanhos14@gmail.com>
2023-04-30 13:51:19 -0700

    Fresh slate

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -12,8 +12,7 @@ export default function NavBar({ user, setUser }) {
       <Link to="/orders">Order History</Link>
       &nbsp; | &nbsp;
       <Link to="/orders/new">New Order</Link>
-      &nbsp;&nbsp;
-      <span>Welcome, {user.name}</span>
+      &nbsp;|
       &nbsp;&nbsp;<Link to="" onClick={handleLogOut}>Log Out</Link>
     </nav>
   );
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,29 +1,18 @@
-import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { getUser } from '../../utilities/users-service';
 import './App.css';
-import AuthPage from '../AuthPage/AuthPage';
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
-import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 import NavBar from '../../components/NavBar/NavBar';
+import HomePage from './HomePage/HomePage';
 
 export default function App() {
-  const [user, setUser] = useState(getUser());
 
   return (
     <main className="App">
-      { user ?
           <>
-            <NavBar user={user} setUser={setUser} />
+            <NavBar />
             <Routes>
-              {/* Route components in here */}
-              <Route path="/orders/new" element={<NewOrderPage />} />
-              <Route path="/orders" element={<OrderHistoryPage />} />
+              <Route path="/" element={<HomePage />}/>
             </Routes>
           </>
-          :
-          <AuthPage setUser={setUser} />
-      }
     </main>
   );
 }
