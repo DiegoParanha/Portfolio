f6944c5035724279c0551b42bd19119af8246d2e
DiegoParanha <DiegoParanhos14@gmail.com>
2023-05-08 21:23:07 -0700

    fixed about me for mobile

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom';
-// import * as userService from '../../utilities/users-service';
+import * as userService from '../../utilities/users-service';
 import './NavBar.css';
 
 export default function NavBar({ user, setUser }) {
-  // function handleLogOut() {
-  //   userService.logOut();
-  //   setUser(null);
-  // }
+  function handleLogOut() {
+    userService.logOut();
+    setUser(null);
+  }
 
   return (
     <nav>
