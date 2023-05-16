893e0889e25017d7ee4a95d4f506479d887bfec5
DiegoParanha <DiegoParanhos14@gmail.com>
2023-05-15 18:04:39 -0700

    added email

--- src/pages/App/ContactMe/ContactMe.jsx
+++ src/pages/App/ContactMe/ContactMe.jsx
@@ -9,10 +9,12 @@ export default function ContactMe() {
         <h2 className='underTitle'>I look forward to hearing from you!</h2>
         <div className='contact-background'>
             <div className='contact-form'>
+                <form className="contact-form2" method="post" action="mailto:diegoparanhos14@gmail.com" >
                 <input name="name" placeholder="Enter Your Full name" id="name-input"></input>
                 <input name="email" placeholder="Enter Your email" id="email-input"></input>
-                <textarea name="description" placeholder="Message"id="" cols="30" rows="10"></textarea>
-                <button className='contact-submit'>Submit</button>
+                <textarea name="description" placeholder="Leave a detailed Message"id="" cols="30" rows="10"></textarea>
+                <input className="contact-submit" type="submit" value="Send an Email" /> 
+                </form>
             </div>
         </div>
         <Footer />
